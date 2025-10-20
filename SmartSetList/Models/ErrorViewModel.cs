7631ff065f34995874fdd2cee898116a84d681ee
unknown <Suporte@GSNT-008.corp.bmggranito.com.br>
2025-10-20 15:55:04 -0300

    First

---
+++ SmartSetList/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SmartSetList.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
